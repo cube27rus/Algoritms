938526910322304cc20c203091ad5b0650df757a
Igor Bondarenko <Npro27@yandex.ru>
2017-08-29 16:54:03 +1000

    Update Haffaman.cs

--- Aug/Haffaman.cs
+++ Aug/Haffaman.cs
@@ -1,19 +1,14 @@
-//Rextester.Program.Main is the entry point for your code. Don't change it.
-//Compiler version 4.0.30319.17929 for Microsoft (R) .NET Framework 4.5
-
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text.RegularExpressions;
 
-namespace Rextester
+public class MainClass
 {
-    public class Program
+    public static void Main()
     {
-        public static void Main(string[] args)
-        {
-             string target = Console.ReadLine();
-            Console.WriteLine(target);
+          string target = Console.ReadLine();
+            //Console.WriteLine(target);
             Dictionary<char, string> words = new Dictionary<char, string>();
             String curWay = "0";
            for (char c = 'a'; c <= 'z'; c++)
@@ -23,18 +18,38 @@ namespace Rextester
                words.Add(chares,curWay);
                    curWay="1"+curWay;               
             } 
-            /*foreach(KeyValuePair<string, string> entry in words){
+            //мапа с уникальными занчениями
+            SortedDictionary<char, string> targetDict = new SortedDictionary<char, string>();
+            
+            char[] targetToCharArray = target.ToCharArray();
+            //формируем уникальную мапу
+            for(int i = 0;i<targetToCharArray.Count();i++){
+                if(!targetDict.ContainsKey(targetToCharArray[i])){
+                    targetDict.Add(targetToCharArray[i],words[targetToCharArray[i]]);
+                }                
+            }
+             if(targetDict.Count>1){
+            var last = targetDict.Values.Last();
+            var lastKey = targetDict.Keys.Last();
+            targetDict.Remove(targetDict.Keys.Last());
+            targetDict.Add(lastKey, last.Remove(last.Length - 1));
+            }
+            
+            /*foreach(KeyValuePair<char, string> entry in targetDict){
                 Console.WriteLine("key : {0} + value : {1}", entry.Key,entry.Value);
             }*/
-            char[] targetToCharArray = target.ToCharArray();
             
+            Console.Write(targetDict.Count()+" ");
             String result = "";
             for(int i = 0;i<targetToCharArray.Count();i++){
-                Console.WriteLine("{0} : {1}", targetToCharArray[i],words[targetToCharArray[i]]);
-                 result +=words[targetToCharArray[i]];
+                //Console.WriteLine("{0} : {1}", targetToCharArray[i],targetDict[targetToCharArray[i]]);
+                 result +=targetDict[targetToCharArray[i]];
             }
             Console.WriteLine(result.Length);
+            foreach(KeyValuePair<char, string> entry in targetDict){
+                Console.WriteLine("{0}: {1}", entry.Key,entry.Value);
+            }
+           
             Console.WriteLine(result);
-        }
     }
 }
